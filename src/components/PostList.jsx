27010c59c0955e790c7a93825750215fd5b7990a
KumarYash07 <yash.23b0101151@abes.ac.in>
2026-06-23 16:56:23 +0530

    Initalize Loading Page

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/PostList-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
@@ -19,17 +20,33 @@ const PostList = () => {
   // }
 
   // Fetching posts using UseEffect()
+  // useEffect(()=>{
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then(data => {
+  //       addInitialPosts(data.posts);
+  //     });
+  // }, []);
+
+
+  const [fetching, setFetching] = useState(false);
   useEffect(()=>{
+    setFetching(true);
+    console.log("Fetch started");
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then(data => {
         addInitialPosts(data.posts);
+        setFetching(false);
+        console.log("Fetch returned");
       });
+      console.log("Fetch ended");
   }, []);
 
 
   return (
     <>
+      {/*fetching && */ <LoadingSpinner></LoadingSpinner>}
       {postList.length === 0 && (
         <WelcomeMessage></WelcomeMessage>
       )}
